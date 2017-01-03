253b95ec061f70fceddd518d6468f27084ec4265
kp <kpisgod@gmail.com>
2017-01-04 00:57:40 +0530

    add mortal contract

---
+++ contracts/mortal.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.0;
+contract mortal {
+    /* Define variable owner of the type address*/
+    address owner;
+
+    /* this function is executed at initialization and sets the owner of the contract */
+    function mortal() { owner = msg.sender; }
+
+    /* Function to recover the funds on the contract */
+    function kill() { if (msg.sender == owner) selfdestruct(owner); }
+}
