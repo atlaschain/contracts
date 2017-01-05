cd373236fa9a8f8113c3bdc1f430e015c4bd5242
kp <kpisgod@gmail.com>
2017-01-05 11:43:07 +0530

    fix arguement error

--- contracts/Profile.sol
+++ contracts/Profile.sol
@@ -10,56 +10,61 @@ contract Profile{
   }
   mapping(address => Info) profiles;
 
-  function add_my_info(string _name, string _public_key,address[] _subscriptions, address[] _departments){
-    profiles[msg.sender] = Info({
-      name: _name,
-      public_key: _public_key,
-      subscriptions: _subscriptions,
-      departments: _departments
-      });
-    }
+  function add_my_info(string _name,
+    string _public_key,
+    address[] _subscriptions,
+    address[] _departments,
+    address[] _owned_departments){
+      profiles[msg.sender] = Info({
+        name: _name,
+        public_key: _public_key,
+        subscriptions: _subscriptions,
+        departments: _departments,
+        owned_departments: _owned_departments
+        });
+      }
 
-    function add_subscription(address _user, address _subscription) returns(uint){
-      return profiles[_user].subscriptions.push(_subscription);
-    }
+      function add_subscription(address _user, address _subscription) returns(uint){
+        return profiles[_user].subscriptions.push(_subscription);
+      }
 
-    function add_department(address _user, address _department) returns(uint){
-      return profiles[_user].departments.push(_department);
-    }
-    function add_owned_department(address _user, address _department) returns(uint){
-      return profiles[_user].owned_departments.push(_department);
-    }
+      function add_department(address _user, address _department) returns(uint){
+        return profiles[_user].departments.push(_department);
+      }
+      function add_owned_department(address _user, address _department) returns(uint){
+        return profiles[_user].owned_departments.push(_department);
+      }
 
-    function name(address _user) returns(string _name){
-      return profiles[_user].name;
-    }
+      function name(address _user) returns(string _name){
+        return profiles[_user].name;
+      }
 
-    function public_key(address _user) returns (string _key){
-      return profiles[_user].public_key;
-    }
+      function public_key(address _user) returns (string _key){
+        return profiles[_user].public_key;
+      }
 
-    function subscriptions(address _user) returns(address[] _subscriptions){
-      return profiles[_user].subscriptions;
-    }
+      function subscriptions(address _user) returns(address[] _subscriptions){
+        return profiles[_user].subscriptions;
+      }
 
-    function departments(address _user) returns(address[] _departments){
-      return profiles[_user].departments;
-    }
-    function owned_departments(address _user) returns(address[] _departments){
-      return profiles[_user].owned_departments;
-    }
+      function departments(address _user) returns(address[] _departments){
+        return profiles[_user].departments;
+      }
+      function owned_departments(address _user) returns(address[] _departments){
+        return profiles[_user].owned_departments;
+      }
 
-    function indexOf(address[] values, address _x)returns (uint){
-      for (uint i = 0; i < values.length; i++)
-      if (values[i] == _x) return i;
-      return uint(-1);
+      function indexOf(address[] values, address _x)returns (uint){
+        for (uint i = 0; i < values.length; i++)
+        if (values[i] == _x) return i;
+        return uint(-1);
+      }
+      /*Doesnt work TODO*/
+      function remove_subscription(address _subscription, address _user)returns(bool){
+        uint index = indexOf(profiles[_user].subscriptions, _subscription);
+        if(index == uint(-1))
+        return false;
+        delete profiles[_user].subscriptions[index];
+        return true;
+      }
     }
-    /*Doesnt work TODO*/
-    function remove_subscription(address _subscription, address _user)returns(bool){
-      uint index = indexOf(profiles[_user].subscriptions, _subscription);
-      if(index == uint(-1))
-      return false;
-      delete profiles[_user].subscriptions[index];
-      return true;
-    }
-  }
