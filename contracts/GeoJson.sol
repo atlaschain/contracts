418407bb4ebe62909e65c520af941355bc9360bf
kp <kpisgod@gmail.com>
2017-01-05 17:29:50 +0530

    event for client to hook and display new added data

--- contracts/GeoJson.sol
+++ contracts/GeoJson.sol
@@ -3,17 +3,19 @@ import "mortal.sol";
 
 contract GeoJson is mortal{
   address creator;
-
+  event new_json_added(uint _geojson_count, string _datum);
   string[] public geojson;
   function GeoJson(){
     creator = msg.sender;
   }
-  function add_json_datum(string data)returns (uint){
-    return geojson.push(data);
+  function add_json_datum(string datum)returns (uint){
+    uint count = geojson.push(datum);
+    new_json_added(count, datum);
+    return count;
   }
 
   function geojson_count() returns(uint){
     return geojson.length;
   }
-  
+
 }
